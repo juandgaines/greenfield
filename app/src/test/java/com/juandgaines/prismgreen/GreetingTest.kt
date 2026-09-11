3b946b5be7600761c83d778aa70cad7b709571b8
juandgaines <juandavidgaines@gmail.com>
2026-09-11 13:07:09 -0500

    add a test

---
+++ app/src/test/java/com/juandgaines/prismgreen/GreetingTest.kt
@@ -0,0 +1,12 @@
+package com.juandgaines.prismgreen
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class GreetingTest {
+
+    @Test
+    fun greetingComposableExists() {
+        assertEquals("Android", "Android")
+    }
+}
